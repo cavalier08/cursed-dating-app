6a839d5593b35b5fe15ae645f779d944fedbbc89
Chantal <chantal.zhang2018@gmail.com>
2025-06-19 17:07:19 -0400

    updated ui of login page it still looks cursed pls dont judge

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import '../app/globals.css';
+
+export default function Footer() {
+    return (
+        <div className="fixed bottom-0 bg-slate-900 h-10 w-full flex justify-center items-center">
+            <span>Made by <a className="hover:text-pink" target="_blank" href="https://github.com/cavalier08/cursed-dating-app">&lt;3</a></span>
+
+        </div>
+    );
+
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,17 +1,30 @@
 import Link from "next/link";
+import Footer from "../components/footer"
 import '../app/globals.css';
 
 export default function Login() {
   return (
-    <div>
-        <p>Username:</p>
-        <input type="text"></input>
-        <p>Password:</p>
-        <input type="text"></input>
+    <div className="">
+      <h1 className="flex justify-center items-center text-3xl m-10"
+      >the-cursed-dating-app</h1>
+      <div >
+        <LoginFields fieldName="Password"></LoginFields>
+        <LoginFields fieldName="Username"></LoginFields>
         <Link
             href={"/matches"}>
             <p>Log In</p>
         </Link>
+      </div>
+        <Footer></Footer>
+    </div>
+  );
+}
+
+function LoginFields({fieldName}: {fieldName: string}) {
+  return(
+    <div className="w-1/2 bg-slate-900">
+      <label>{fieldName}:</label>
+      <input className="text-gray-300 p-2 rounded-sm bg-slate-700 w-full" type="text"></input>
     </div>
   );
 }
