b9d2ae685f794cd3e59d61513fdc3d53b7155a3f
Chantal <chantal.zhang2018@gmail.com>
2025-06-20 16:40:50 -0400

    ui changes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,16 +4,24 @@ import '../app/globals.css';
 
 export default function Login() {
   return (
-    <div className="">
+    <div className="place-items-center grid h-screen">
       <h1 className="flex justify-center items-center text-3xl m-10"
       >the-cursed-dating-app</h1>
-      <div className="w-full">
+      
+      <div className="p-4 bg-slate-800">
         <LoginFields fieldName="Password"></LoginFields>
         <LoginFields fieldName="Username"></LoginFields>
-        <Link 
-            href={"/matches"}>
-            <button className="ml-4 w-1/4 bg-slate-700 hover:bg-slate-500 rounded-sm p-1">Log In</button>
-        </Link>
+        <div className="w-full">
+          <Link 
+              href={"/matches"}>
+              <button className="w-1/4 bg-slate-700 hover:bg-slate-500 rounded-sm p-1 ">Log In</button>
+          </Link>
+          <Link 
+              href={"/matches"}>
+              <button className="w-1/4 bg-blue-900 hover:bg-slate-500 rounded-sm p-1">Sign Up</button>
+          </Link>
+        </div>
+        
       </div>
         <Footer></Footer>
     </div>
@@ -22,7 +30,7 @@ export default function Login() {
 
 function LoginFields({fieldName}: {fieldName: string}) {
   return(
-    <div className="w-1/2 bg-slate-900 m-4">
+    <div className="w-1/2 mb-4">
       <label>{fieldName}:</label>
       <input className="text-gray-300 p-2 rounded-sm bg-slate-700 hover:bg-slate-600 w-full" type="text"></input>
     </div>
