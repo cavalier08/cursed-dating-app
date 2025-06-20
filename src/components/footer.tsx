93de733abbbe1224994248ae0edbd153095fd69f
Chantal <chantal.zhang2018@gmail.com>
2025-06-20 16:27:18 -0400

    ui changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import '../app/globals.css';
 export default function Footer() {
     return (
         <div className="fixed bottom-0 bg-slate-900 h-10 w-full flex justify-center items-center">
-            <span>Made by <a className="hover:text-pink" target="_blank" href="https://github.com/cavalier08/cursed-dating-app">&lt;3</a></span>
+            <span>Made by <a className="hover:text-pink-300" target="_blank" href="https://github.com/cavalier08/cursed-dating-app">&lt;3</a></span>
 
         </div>
     );
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,12 +7,12 @@ export default function Login() {
     <div className="">
       <h1 className="flex justify-center items-center text-3xl m-10"
       >the-cursed-dating-app</h1>
-      <div className="">
+      <div className="w-full">
         <LoginFields fieldName="Password"></LoginFields>
         <LoginFields fieldName="Username"></LoginFields>
-        <Link
+        <Link 
             href={"/matches"}>
-            <p>Log In</p>
+            <button className="ml-4 w-1/4 bg-slate-700 hover:bg-slate-500 rounded-sm p-1">Log In</button>
         </Link>
       </div>
         <Footer></Footer>
@@ -22,9 +22,9 @@ export default function Login() {
 
 function LoginFields({fieldName}: {fieldName: string}) {
   return(
-    <div className="w-1/2 bg-slate-900">
+    <div className="w-1/2 bg-slate-900 m-4">
       <label>{fieldName}:</label>
-      <input className="text-gray-300 p-2 rounded-sm bg-slate-700 w-full" type="text"></input>
+      <input className="text-gray-300 p-2 rounded-sm bg-slate-700 hover:bg-slate-600 w-full" type="text"></input>
     </div>
   );
 }
